e839839143a05bb478be6c3f31f428339e623687
Lizan Pradhanang <lizanpradhanang@apples-MacBook-Pro.local>
2017-05-07 16:46:14 +0545

    Initial Commit

---
+++ GMaps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GMaps
+//
+//  Created by Lizan Pradhanang on 5/7/17.
+//  Copyright © 2017 Lizan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
